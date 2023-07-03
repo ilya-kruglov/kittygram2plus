91636d2be2de0eb8131dbbe6d8fdf74ccc2615ed
Ilya Kruglov <ilya.kruglove@yandex.ru>
2023-07-03 22:25:05 +0300

    Add permission to CatViewSet (IsAuthenticatedOrReadOnly)

--- cats/views.py
+++ cats/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import permissions, viewsets
 
 from .models import Achievement, Cat, User
 
@@ -8,9 +8,10 @@ from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+        serializer.save(owner=self.request.user)
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
